6ceb3590dd0dfff1238e59f79dcbcdf4b67663a2
Arnav Gupta <info@arnavgupta.net>
2022-10-23 17:59:13 +0530

    merge

--- deploy.js
+++ deploy.js
@@ -1,4 +1,24 @@
-async function main() {}
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "17c7edd997e707a848b6bab5fbdec150ea73171fc0414ad49d153e165b9d98fc",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  const contract = await contractFactory.deploy();
+  const deploymentReciept = await contract.deployTransaction.wait(1);
+  console.log(deploymentReciept);
+}
 main()
   .then(() => process.exit(0))
   .catch((e) => {
